cfca793887e64629355bc14acfc9cabb23759bc3
Rahul <mrsvaz@icloud.com>
2024-04-16 13:14:05 +0530

    single

--- lib/features/start_up_page/start_page.dart
+++ lib/features/start_up_page/start_page.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_services_with_bloc/constant/size/sized.dart';
 import 'package:firebase_services_with_bloc/features/admin_page/admin_page.dart';
@@ -24,132 +23,134 @@ class StartPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset('assets/animation/campus.json'),
-            Text(
-              'Welcome Back',
-              style: TextStyle(
-                  fontSize: width * 0.07, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: height * 0.03,
-            ),
-            Text(
-              'Choose The Type of Account ',
-              style: TextStyle(fontSize: width * 0.04),
-            ),
-            SizedBox(
-              height: height * 0.03,
-            ),
-            AuthButton(
-              label: "Alumni",
-              callback: () {
-                (FirebaseAuth.instance.currentUser == null)
-                    ? Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlumniLoginPage(),
-                        ),
-                      )
-                    : Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlumniHomeScreen(),
-                        ),
-                        (route) => false);
-              },
-            ),
-            SizedBox(
-              height: height * 0.03,
-            ),
-            AuthButton(
-              label: "Student",
-              callback: () {
-                (FirebaseAuth.instance.currentUser == null)
-                    ? Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const StudentLoginPage(),
-                        ),
-                      )
-                    : Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const StudentGNav(),
-                        ),
-                        (route) => false);
-              },
-            ),
-            const Height20(),
-            TextButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => Material(
-                    child: Center(
-                      child: SingleChildScrollView(
-                        child: SizedBox(
-                          height: height * 0.3,
-                          width: width * 0.7,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text('Please Enter Your Security Code'),
-                              const Height20(),
-                              CustomTextField(
-                                controller: codeController,
-                                label: 'Security code',
-                              ),
-                              const Height20(),
-                              AuthButton(
-                                label: 'Login As admin',
-                                callback: () async {
-                                  if (codeController.text.trim() == '2662') {
-                                    Navigator.pushAndRemoveUntil(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const AdminPage(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset('assets/animation/campus.json'),
+              Text(
+                'Welcome Back',
+                style: TextStyle(
+                    fontSize: width * 0.07, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              Text(
+                'Choose The Type of Account ',
+                style: TextStyle(fontSize: width * 0.04),
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              AuthButton(
+                label: "Alumni",
+                callback: () {
+                  (FirebaseAuth.instance.currentUser == null)
+                      ? Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const AlumniLoginPage(),
+                          ),
+                        )
+                      : Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const AlumniHomeScreen(),
+                          ),
+                          (route) => false);
+                },
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              AuthButton(
+                label: "Student",
+                callback: () {
+                  (FirebaseAuth.instance.currentUser == null)
+                      ? Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const StudentLoginPage(),
+                          ),
+                        )
+                      : Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const StudentGNav(),
+                          ),
+                          (route) => false);
+                },
+              ),
+              const Height20(),
+              TextButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => Material(
+                      child: Center(
+                        child: SingleChildScrollView(
+                          child: SizedBox(
+                            height: height * 0.3,
+                            width: width * 0.7,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Please Enter Your Security Code'),
+                                const Height20(),
+                                CustomTextField(
+                                  controller: codeController,
+                                  label: 'Security code',
+                                ),
+                                const Height20(),
+                                AuthButton(
+                                  label: 'Login As admin',
+                                  callback: () async {
+                                    if (codeController.text.trim() == '2662') {
+                                      Navigator.pushAndRemoveUntil(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const AdminPage(),
+                                          ),
+                                          (route) => false);
+                                      try {
+                                        await FirebaseAuth.instance
+                                            .signInWithEmailAndPassword(
+                                                email: 'admin@gmial.com',
+                                                password: 'password');
+                                      } catch (e) {
+                                        log(e.toString());
+                                      }
+                                    } else {
+                                      Navigator.pop(context);
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                          content:
+                                              Text('Incorrect Security Code'),
                                         ),
-                                        (route) => false);
-                                    try {
-                                      await FirebaseAuth.instance
-                                          .signInWithEmailAndPassword(
-                                              email: 'admin@gmial.com',
-                                              password: 'password');
-                                    } catch (e) {
-                                      log(e.toString());
+                                      );
                                     }
-                                  } else {
+                                  },
+                                ),
+                                TextButton(
+                                  onPressed: () {
                                     Navigator.pop(context);
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        content:
-                                            Text('Incorrect Security Code'),
-                                      ),
-                                    );
-                                  }
-                                },
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('Tap Here To Go Back'),
-                              )
-                            ],
+                                  },
+                                  child: const Text('Tap Here To Go Back'),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-              child: const Text('Login As Admin'),
-            )
-          ],
+                  );
+                },
+                child: const Text('Login As Admin'),
+              )
+            ],
+          ),
         ),
       ).animate().fade(delay: 500.ms, duration: 500.ms),
     );
