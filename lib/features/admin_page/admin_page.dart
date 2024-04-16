7469138ae0f3212b788a0b4ddd08b116b1523a31
Rahul <mrsvaz@icloud.com>
2024-04-16 10:44:44 +0530

    added

--- lib/features/admin_page/admin_page.dart
+++ lib/features/admin_page/admin_page.dart
@@ -7,11 +7,9 @@ import 'package:flutter/material.dart';
 
 class AdminPage extends StatefulWidget {
   const AdminPage({super.key});
-
   @override
   State<AdminPage> createState() => _AdminPageState();
 }
-
 class _AdminPageState extends State<AdminPage> {
   @override
   Widget build(BuildContext context) {
